a5e83a69cbe81f2da380829bca1531e9fa06873e
Victor <victorlangnier2@gmail.com>
2022-02-22 19:11:59 -0300

    Arquivo criado

---
+++ Charpter.WebApi/Charpter.WebApi/Contexts/CharpterContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Charpter.WebApi.Models;
+
+namespace Charpter.WebApi.Contexts
+{
+    public class CharpterContext : DbContext
+    {
+        public CharpterContext()
+        { 
+        }
+
+        public CharpterContext(DbContextOptions<CharpterContext> options) : base(options)
+        {
+            
+        }
+
+        // vamos utilizar esse método para configurar o banco de dados
+        protected override void
+            OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-JTA7A8U; initial catalog = Chapter; Integrated Security = true");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro>? Livros { get; set; }
+    }
+}
---
+++ Charpter.WebApi/Charpter.WebApi/Controllers/LivroController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Charpter.WebApi.Repositories;
+
+namespace Charpter.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+        [HttpGet]
+
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livroRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return Ok();
+        }
+    }
+}
